6b6b9d7cc3ecc39a40b7b41f46dc4e67687b3cff
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 22:50:42 -0500

    Remve cpp prefix

--- include/collections/forward_list.hpp
+++ include/collections/forward_list.hpp
@@ -1,4 +1,16 @@
-#ifndef CPP_FORWARD_LIST_HPP
-#define CPP_FORWARD_LIST_HPP
+#ifndef COLLECTIONS_FORWARD_LIST_HPP
+#define COLLECTIONS_FORWARD_LIST_HPP
 
-#endif // #ifndef CPP_FORWARD_LIST_HPP
+// ISO C Includes
+#include <cstddef>
+
+// ISO C++ Includes
+#include <memory>
+
+namespace collections {
+    template<typename T, typename Allocator = std::allocator<T>>
+    class forward_list {
+    };
+} // namespace collections
+
+#endif // #ifndef COLLECTIONS_FORWARD_LIST_HPP
